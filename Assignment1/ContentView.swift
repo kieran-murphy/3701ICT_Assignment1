e5b593cd81e5153cae6eeda261f30697a5b6badd
Kieran <kmurp62rulz@gmail.com>
2021-03-17 22:38:47 +1000

    Add first image, title, description, story

--- Assignment1/ContentView.swift
+++ Assignment1/ContentView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading) {
+            Image("cottonCandy")
+                .resizable()
+                //.scaledToFit()
+                .frame(width: 414.0,height:400)
+            VStack(alignment: .leading) {
+                Text("Cotton Candy")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.leading)
+                    .padding(.leading)
+                Text("Ingredients: Sugar, Happiness")
+                    .italic()
+                    .fontWeight(.ultraLight)
+                    .multilineTextAlignment(.leading)
+                    .padding(.leading)
+                Text("My dentist hates me for eating this. Sorry, dentist.")
+                    .padding(.leading)
+                    .padding(.top)
+            }
+            
+        }.padding(.bottom).frame(minWidth: 0,
+                maxWidth: 180,
+                minHeight: 0,
+                maxHeight: .infinity,
+                alignment: .center
+        )
     }
 }
 
