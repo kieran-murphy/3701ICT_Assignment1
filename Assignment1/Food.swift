7e1f0b2f76023baa4c04c92dcf980d04a5c01b50
kmurp62rulz <kmurp62rulz@gmail.com>
2021-03-24 10:49:32 +1000

    Streamline tests, add proper Swift documentation

--- Assignment1/Food.swift
+++ Assignment1/Food.swift
@@ -5,13 +5,29 @@
 //  Created by Kieran Murphy on 21/3/21.
 //
 
+
+/// 🍔 An edible form of energy for humans
 class Food {
+    ///The name of the food item
     var name: String
+    ///The reference image of the food item
     var image: String
+    ///A short description of the food item
     var desc: String
+    ///A short personal story about the food item
     var story: String
     
-    
+    /**
+         Initializes a new food with the provided attributes.
+
+         - Parameters:
+            - name: The name of the food item
+            - image: The reference image of the food item
+            - desc: A short description of the food item
+            - story: A short personal story about the food item
+
+         - Returns: A yummy food item constructed in Swift
+         */
     init(name: String, image: String, desc: String, story: String) {
         self.name = name
         self.image = image
@@ -21,4 +37,8 @@ class Food {
     }
 }
 
+
 let cottonCandy = Food(name: "Cotton Candy", image: "cottonCandy", desc: "A mixture of sugar and happiness", story: "My dentist hates me for eating this. Sorry, dentist.")
+
+
+
--- Assignment1Tests/Assignment1Tests.swift
+++ Assignment1Tests/Assignment1Tests.swift
@@ -8,54 +8,67 @@
 import XCTest
 @testable import Assignment1
 
+/// A class to contain all the tests for the application
 class Assignment1Tests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        /// Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        /// Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testFood() {
-        let name = "Cotton Candy"
-        let image = "cottonCandy"
-        let desc = "A mixture of sugar and happiness"
-        let story = "My dentist hates me for eating this. Sorry, dentist."
-        let food = Food(name: name, image: image, desc: desc, story: story)
-        
-        XCTAssertEqual(food.image, image)
-    }
+    /**
+         Runs a set of XCTAssertEqual tests.
 
-    func testFoodName() {
-        let name = "Cotton Candy"
-        let image = "cottonCandy"
-        let desc = "A mixture of sugar and happiness"
-        let story = "My dentist hates me for eating this. Sorry, dentist."
-        let food = Food(name: name, image: image, desc: desc, story: story)
-        
-        XCTAssertEqual(food.name, name)
-    }
+         - Parameters:
+            - name: The name of the food item
+            - image: The reference image of the food item
+            - desc: A short description of the food item
+            - story: A short personal story about the food item
+         - Returns:
+            For each parameter runs an XCTAssertEqual test, testing the local variables within the method against the associated attributes in the "Food" class
+         */
+        func testFoodGetters() {
+            let name = "Cotton Candy"
+            let image = "cottonCandy"
+            let desc = "A mixture of sugar and happiness"
+            let story = "My dentist hates me for eating this. Sorry, dentist."
+            let food = Food(name: name, image: image, desc: desc, story: story)
+            
+            XCTAssertEqual(food.image, image)
+            XCTAssertEqual(food.name, name)
+            XCTAssertEqual(food.desc, desc)
+            XCTAssertEqual(food.story, story)
+        }
     
-    func testFoodStory() {
-        let name = "Cotton Candy"
-        let image = "cottonCandy"
-        let desc = "A mixture of sugar and happiness"
-        let story = "My dentist hates me for eating this. Sorry, dentist."
-        let food = Food(name: name, image: image, desc: desc, story: story)
         
-        XCTAssertEqual(food.story, story)
-    }
-    
-    func testFoodDesc() {
-        let name = "Cotton Candy"
-        let image = "cottonCandy"
-        let desc = "A mixture of sugar and happiness"
-        let story = "My dentist hates me for eating this. Sorry, dentist."
-        let food = Food(name: name, image: image, desc: desc, story: story)
-        
-        XCTAssertEqual(food.desc, desc)
-    }
-
+        func testFoodSetters() {
+            let name = "Cotton Candy"
+            let image = "cottonCandy"
+            let desc = "A mixture of sugar and happiness"
+            let story = "My dentist hates me for eating this. Sorry, dentist."
+            let food = Food(name: name, image: image, desc: desc, story: story)
+            
+            let newName = "Frozen Yoghurt"
+            food.name = newName
+            XCTAssertEqual(food.name, "Frozen Yoghurt")
+            
+            let newImage = "Frozen Yoghurt Image"
+            food.image = newImage
+            XCTAssertEqual(food.image, "Frozen Yoghurt Image")
+            
+            let newDesc = "Frozen Yoghurt Description"
+            food.desc = newDesc
+            XCTAssertEqual(food.desc, "Frozen Yoghurt Description")
+            
+            let newStory = "Frozen Yoghurt Story"
+            food.story = newStory
+            XCTAssertEqual(food.story, "Frozen Yoghurt Story")
+        }
 }
+
+
+
+
